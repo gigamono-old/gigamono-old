f7000cd771a17a195875eecb45713a0b6e8b325e
Stephen <appcypher@outlook.com>
2021-03-14 17:24:20 +0100

    Make app initialisation fail fast

--- pkg/inits/app.go
+++ pkg/inits/app.go
@@ -1,12 +1,15 @@
 package inits
 
 import (
+	"fmt"
+
 	"github.com/sageflow/sageflow/pkg/configs"
 	"github.com/sageflow/sageflow/pkg/database"
 	"github.com/sageflow/sageflow/pkg/logs"
 	"github.com/sageflow/sageflow/pkg/secrets"
 )
 
+// App holds common important states of a service.
 type App struct {
 	Config  configs.SageflowConfig
 	Secrets secrets.Manager
@@ -22,18 +25,24 @@ func NewApp(appKind string) (App, error) {
 	// Load sageflow config file.
 	config, err := configs.LoadSageflowConfig()
 	if err != nil {
-		logs.FmtPrintln("Unable to load sageflow config file from env var `SAGEFLOW_CONFIG_FILE`:", err)
+		err := fmt.Errorf("initialising app: unable to load sageflow config file from env var `SAGEFLOW_CONFIG_FILE`: %v", err)
+		logs.FmtPrintln(err)
+		return App{}, err
 	}
 
 	// Set up secret manager,
 	secrets, err := secrets.NewManager(&config)
 	if err != nil {
-		logs.FmtPrintln("Unable to create secrets manager:", err)
+		err := fmt.Errorf("initialising app: unable to create a secrets manager: %v", err)
+		logs.FmtPrintln(err)
+		return App{}, err
 	}
 
 	// Connect to database.
 	db, err := database.Connect(&config, secrets, appKind)
 	if err != nil {
+		err := fmt.Errorf("initialising app: unable to connect to db: %v", err)
+		logs.FmtPrintln(err)
 		return App{}, err
 	}
 
