9d55cf740b0c4ed7814e62aa0b24825e22318792
Stephen <appcypher@outlook.com>
2021-04-27 11:07:39 +0100

    Reduce number of models

--- pkg/database/models/resource/project.go
+++ pkg/database/models/resource/project.go
@@ -5,11 +5,11 @@ import (
 	"github.com/gigamono/gigamono/pkg/database/models"
 )
 
-// Group represents a group.
-type Group struct {
+// Project represents a project under a workspace.
+type Project struct {
 	models.Base
 	Name        string
 	Avatar32URL string `gorm:"column:avatar_32_url"`
 	CreatorID   uuid.UUID
-	XUser       []*User `gorm:"many2many:users_x_groups"`
+	WorkspaceID   uuid.UUID
 }
--- pkg/database/models/resource/user.go
+++ pkg/database/models/resource/user.go
@@ -1,25 +1,16 @@
 package resource
 
 import (
-	"github.com/gofrs/uuid"
 	"github.com/gigamono/gigamono/pkg/database/models"
+	"github.com/gofrs/uuid"
 )
 
 // User stores information about the user.
 type User struct {
 	models.Base
-	AuthUserID   *uuid.UUID `gorm:"unique; type:uuid"`
-	Profile      Profile
-	RefreshToken string // JWT.1.R
-	Account      []Account
-	RESTHook     []RESTHook
-	AppID        []App        `gorm:"foreignKey:CreatorID"`
-	Group        []Group      `gorm:"foreignKey:CreatorID"`
-	Workflow     []Workflow   `gorm:"foreignKey:CreatorID"`
-	Workspace    []Workspace  `gorm:"foreignKey:CreatorID"`
-	Folder       []Folder     `gorm:"foreignKey:CreatorID"`
-	Theme        []Theme      `gorm:"foreignKey:CreatorID"`
-	XGroup       []*Group     `gorm:"many2many:users_x_groups"`
-	XWorkspace   []*Workspace `gorm:"many2many:users_x_workspaces"`
-	XRole        []*Role      `gorm:"many2many:users_x_roles"`
+	AuthUserID *uuid.UUID `gorm:"unique; type:uuid"`
+	Profile    Profile
+	Workspace  []Workspace  `gorm:"foreignKey:CreatorID"`
+	Project    []Project    `gorm:"foreignKey:CreatorID"`
+	XWorkspace []*Workspace `gorm:"many2many:users_x_workspaces"`
 }
