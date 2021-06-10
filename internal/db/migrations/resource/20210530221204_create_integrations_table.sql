e5a6be546e7ff6aa83f26bf2fe6fbb9d7d17a5d1
Stephen <appcypher@outlook.com>
2021-06-10 13:10:16 +0100

    Add integration table migration

--- internal/db/migrations/resource/20210530221204_create_integrations_table.sql
+++ internal/db/migrations/resource/20210530221204_create_integrations_table.sql
@@ -1,16 +1,14 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-CREATE TABLE workflows (
+CREATE TABLE integrations (
     id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     created_at timestamp with time zone DEFAULT (now() at time zone 'utc'),
     updated_at timestamp with time zone,
     deleted_at timestamp with time zone,
     name text,
-    is_active boolean DEFAULT FALSE,
     specification_path text,
-    serverless_js_path text,
     creator_id uuid REFERENCES users(id)
 );
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
-DROP TABLE workflows;
+DROP TABLE integrations;
