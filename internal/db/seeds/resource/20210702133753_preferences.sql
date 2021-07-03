f15f0a05d90d92983c7917d4651356f246eb0d71
Stephen <appcypher@outlook.com>
2021-07-03 16:07:27 +0100

    Add valid details in preferences seed

--- internal/db/seeds/resource/20210702133753_preferences.sql
+++ internal/db/seeds/resource/20210702133753_preferences.sql
@@ -1,5 +1,56 @@
-INSERT INTO preferences (id, details, user_id) VALUES (
+INSERT INTO preferences (id, user_id, details) VALUES (
     '98607144-ddd3-478b-865a-d69ae00fc1cd',
-    '{}',
-    '6da18961-37af-4ee9-afd1-3bddb5ca3e95'
+    '6da18961-37af-4ee9-afd1-3bddb5ca3e95',
+    '{
+        "workspaces": [
+            {
+                "id": "8836df75-b8d3-4f35-b7c3-630e5f38ccdc",
+                "layout": {
+                "main_shortcuts": [
+                    {
+                    "route": "decks",
+                    "icon_name": "decks",
+                    "entity_name": "decks"
+                    },
+                    {
+                    "route": "automations",
+                    "icon_name": "automations",
+                    "entity_name": "automations"
+                    },
+                    {
+                    "route": "bases",
+                    "icon_name": "bases",
+                    "entity_name": "bases"
+                    },
+                    {
+                    "route": "extensions",
+                    "icon_name": "extensions",
+                    "entity_name": "extensions"
+                    }
+                ],
+                "other_shortcuts": [
+                    {
+                    "route": "developers",
+                    "icon_name": "developers",
+                    "entity_name": "developers"
+                    },
+                    {
+                    "route": "settings",
+                    "icon_name": "settings",
+                    "entity_name": "settings"
+                    }
+                ],
+                "quick_shortcuts": [
+                    {
+                    "route": "marketing",
+                    "icon_name": "marketing",
+                    "entity_name": "marketing"
+                    }
+                ]
+                },
+                "spaces": []
+            }
+        ],
+        "focus_workspace_index": 0
+    }'
 );
